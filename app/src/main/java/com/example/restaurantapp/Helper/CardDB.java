065314f8eaa859f974389ea90941a4568ef9c917
SolomonSatoh <solohsatoh0@gmail.com>
2021-08-19 05:10:01 -0700

    (added apk)

--- app/src/main/java/com/example/restaurantapp/Helper/CardDB.java
+++ app/src/main/java/com/example/restaurantapp/Helper/CardDB.java
@@ -1,4 +1,11 @@
 package com.example.restaurantapp.Helper;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class CardDB {
+    private SharedPreferences preferences;
+    private String DEFAULT_APP_IMAGEDATA_DIRECTORY;
+    private String lastImagePath ="";
+
 }
